4cb606c82faeb1d1d48ae3efe413703542d617d2
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-02-27 00:48:09 +0100

    chore: remove obsolete seed, exclude canonical from api prefix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix("api/v1");
+  app.setGlobalPrefix("api/v1", { exclude: ["/"] });
 
   await app.listen(port, "0.0.0.0");
 }
