50e443849bcb89631729450beb1dd3098d009742
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2024-02-26 03:01:57 +0100

    feat: connect and configure database (#5)

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ const port = process.env.PORT || 5000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix("api/v1");
+
   await app.listen(port, "0.0.0.0");
 }
 bootstrap();
