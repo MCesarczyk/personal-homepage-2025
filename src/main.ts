c358991305ccef8474bb08a103b1a24293cbab48
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-03-02 13:33:38 +0100

    docs: differentiate setups between production and development, enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,29 @@ const port = process.env.PORT || 5000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1', { exclude: ['/'] });
+  app.enableCors();
 
-  const options = new DocumentBuilder()
-    .setTitle(`${process.env.DOCS_TITLE || 'Your API Title'}`)
-    .setDescription(`${process.env.DOCS_DESCRIPTION || 'Your API description'}`)
+  const developmentOptions = new DocumentBuilder()
+    .setTitle('Personal Homepage API - development')
+    .setDescription(
+      'Backend for Personal Homepage website. Available on: http://localhost:5000. This API is used to manage the content of the homepage.',
+    )
     .setVersion('1.0')
     .addServer(`http://localhost:${process.env.PORT}/`, 'Development')
     .addServer(`${process.env.PRODUCTION_URL}`, 'Production')
     .build();
+
+  const productionOptions = new DocumentBuilder()
+    .setTitle('Personal Homepage API - production')
+    .setDescription(
+      `Backend for Personal Homepage website. Available on: ${process.env.PRODUCTION_URL || 'PRODUCTION_API URL'}. This API is used to manage the content of the homepage.`,
+    )
+    .setVersion('1.0')
+    .addServer(`${process.env.PRODUCTION_URL}`, 'Production')
+    .build();
+
+  const options =
+    process.env.ENV === 'production' ? productionOptions : developmentOptions;
 
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('docs', app, document);
