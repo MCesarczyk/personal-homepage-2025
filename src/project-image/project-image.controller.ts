efb4341295fe0eb4f5b6198f1118099297bce3b8
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-03-03 19:02:45 +0100

    docs(project-image): add swagger docs

--- src/project-image/project-image.controller.ts
+++ src/project-image/project-image.controller.ts
@@ -12,12 +12,21 @@ import { ProjectImageService } from './project-image.service';
 import { CreateProjectImageDto } from './dto/create-project-image.dto';
 import { UpdateProjectImageDto } from './dto/update-project-image.dto';
 import { ProjectImage } from 'src/project-image/entities/project-image.entity';
+import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiBearerAuth()
+@ApiTags('project-image')
 @Controller('project-image')
 export class ProjectImageController {
   constructor(private readonly projectImageService: ProjectImageService) {}
 
   @Post()
+  @ApiResponse({
+    status: 201,
+    description: 'Added image to project.',
+    type: ProjectImage,
+  })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
   create(
     @Body() createProjectImageDto: CreateProjectImageDto,
   ): Promise<ProjectImage> {
@@ -25,16 +34,34 @@ export class ProjectImageController {
   }
 
   @Get()
+  @ApiResponse({
+    status: 200,
+    description: 'Retrieved all project images.',
+    type: [ProjectImage],
+  })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
   findAll(): Promise<ProjectImage[]> {
     return this.projectImageService.findAll();
   }
 
   @Get(':id')
+  @ApiResponse({
+    status: 200,
+    description: 'Retrieved a project image.',
+    type: ProjectImage,
+  })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
   findOne(@Param('id') id: string): Promise<ProjectImage | null> {
     return this.projectImageService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiResponse({
+    status: 200,
+    description: 'Updated a project image.',
+    type: ProjectImage,
+  })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
   update(
     @Param('id') id: string,
     @Body() updateProjectImageDto: UpdateProjectImageDto,
@@ -43,6 +70,12 @@ export class ProjectImageController {
   }
 
   @Delete(':id')
+  @ApiResponse({
+    status: 200,
+    description: 'Removed a project image.',
+    type: ProjectImage,
+  })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
   remove(@Param('id') id: string): Promise<ProjectImage> {
     return this.projectImageService.remove(id);
   }
