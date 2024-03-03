823ce13b0a2d8885844cdb2cc7371ac3e190731e
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-03-03 02:16:49 +0100

    docs(skill): add swagger docs

--- src/skill/skill.controller.ts
+++ src/skill/skill.controller.ts
@@ -10,33 +10,80 @@ import {
 import { SkillService } from './skill.service';
 import { CreateSkillDto } from './dto/create-skill.dto';
 import { UpdateSkillDto } from './dto/update-skill.dto';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { Skill } from 'src/skill/entities/skill.entity';
 
+@ApiBearerAuth()
+@ApiTags('skill')
 @Controller('skill')
 export class SkillController {
   constructor(private readonly skillService: SkillService) {}
 
   @Post()
-  create(@Body() createSkillDto: CreateSkillDto) {
+  @ApiOperation({ summary: 'Create skill' })
+  @ApiResponse({
+    status: 201,
+    description: 'The skill has been successfully created.',
+    type: Skill,
+  })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
+  create(@Body() createSkillDto: CreateSkillDto): Promise<Skill> {
     return this.skillService.create(createSkillDto);
   }
 
   @Get()
-  findAll() {
+  @ApiOperation({ summary: 'Get all skills' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return all skills.',
+    type: [Skill],
+  })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
+  findAll(): Promise<Skill[]> {
     return this.skillService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  @ApiOperation({ summary: 'Get skill by id' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return skill by id.',
+    type: Skill,
+  })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
+  findOne(@Param('id') id: string): Promise<Skill | null> {
     return this.skillService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSkillDto: UpdateSkillDto) {
+  @ApiOperation({ summary: 'Update skill by id' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return skill updated.',
+    type: Skill,
+  })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
+  update(
+    @Param('id') id: string,
+    @Body() updateSkillDto: UpdateSkillDto,
+  ): Promise<Skill> {
     return this.skillService.update(id, updateSkillDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  @ApiOperation({ summary: 'Delete skill by id' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return skill deleted.',
+    type: Skill,
+  })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
+  remove(@Param('id') id: string): Promise<Skill> {
     return this.skillService.remove(id);
   }
 }
