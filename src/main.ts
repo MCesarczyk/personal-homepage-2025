9755b5d3178610f959b530fc8734684f3e1dcc51
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-03-30 02:16:52 +0100

    fix(auth): add origins explicitly for proper cors handling

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,14 @@ const port = process.env.PORT || 5000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1', { exclude: ['/'] });
-  app.enableCors({ origin: true, credentials: true });
+  app.enableCors({
+    origin: [
+      'https://www.cesarczyk.dev',
+      'https://cesarczyk.dev',
+      'https://home.cesarczyk.dev',
+    ],
+    credentials: true,
+  });
   app.use(cookieParser());
 
   const developmentOptions = new DocumentBuilder()
