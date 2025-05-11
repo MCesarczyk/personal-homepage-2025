90ae9da525ea4368c3ae70eab27494ded1527f06
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2025-05-11 23:27:09 +0200

    fix(project-image): add missing services in module

--- src/project-image/project-image.controller.spec.ts
+++ src/project-image/project-image.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ProjectImageController } from './project-image.controller';
 import { ProjectImageService } from './project-image.service';
 import { PrismaService } from '../prisma.service';
+import { ProjectService } from '../project/project.service';
 
 describe('ProjectImageController', () => {
   let controller: ProjectImageController;
@@ -9,7 +10,7 @@ describe('ProjectImageController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProjectImageController],
-      providers: [ProjectImageService, PrismaService],
+      providers: [ProjectImageService, PrismaService, ProjectService],
     }).compile();
 
     controller = module.get<ProjectImageController>(ProjectImageController);
--- src/project-image/project-image.service.spec.ts
+++ src/project-image/project-image.service.spec.ts
@@ -2,13 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 
 import { ProjectImageService } from './project-image.service';
 import { PrismaService } from '../prisma.service';
+import { ProjectService } from '../project/project.service';
 
 describe('ProjectImageService', () => {
   let service: ProjectImageService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProjectImageService, PrismaService],
+      providers: [ProjectImageService, PrismaService, ProjectService],
     }).compile();
 
     service = module.get<ProjectImageService>(ProjectImageService);
