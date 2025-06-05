e2eddd346e43faa9f02bed26eac0acb609afe271
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2025-06-05 15:13:10 +0200

    ci: add commit linting workflow (#23)
    Resolves: #22

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `<h1>Backend for <a href="https://cesarczyk.dev" >cesarczyk.dev</a> portfolio up and running 🚀</h1>\
+    return `<h1>Backend for <a href="https://cesarczyk.dev" >cesarczyk.dev</a> portfolio up and running 🚀🚀🚀</h1>\
     <p><h3>Docs available at <a href="${process.env.PUBLIC_URL}/docs" >${process.env.PUBLIC_URL}/docs</a></h3></p>`;
   }
 }
