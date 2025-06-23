7d6edc65d8f28121ea7f5b611b78e8b6302b35ea
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2025-06-24 01:24:32 +0200

    ci: update development docker config

--- apps/backend/migrate-and-seed.sh
+++ apps/backend/migrate-and-seed.sh
@@ -6,7 +6,7 @@ echo "Waiting for database..."
 sleep 5
 echo "Running migrations..."
 npx prisma migrate deploy
-npx prisma generate
+sleep 5
 echo "Seeding database..."
 npx prisma db seed
 sleep 5
