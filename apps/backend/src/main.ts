afd20d405cb084290886a455d8973077a74ddcf4
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2025-06-28 21:11:00 +0200

    ci: move env to repo root (#77)
    - rename backend port env name to avoid conflicts
    - move non-secret envs to compose files
    
    Resolves: #76

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
 
-const port = process.env.PORT || 5000;
+const port = process.env.BACKEND_PORT || 5000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
