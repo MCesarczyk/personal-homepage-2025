7cf052eedd82c2ce2a9f4715a9aabd4612c1f64d
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2025-07-27 13:59:35 +0200

    feat(backend): add db health indicator in health check service

--- apps/backend/src/health-check/health-check.module.ts
+++ apps/backend/src/health-check/health-check.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { HealthCheckService } from './health-check.service';
 import { HealthCheckController } from './health-check.controller';
+import { TerminusModule } from '@nestjs/terminus';
+import { HealthCheckService } from 'src/health-check/health-check.service';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
+  imports: [TerminusModule],
   controllers: [HealthCheckController],
-  providers: [HealthCheckService],
+  providers: [HealthCheckService, PrismaService],
 })
 export class HealthCheckModule {}
