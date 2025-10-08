bdd9122ac4d39b5493db656319b1aed7429079c4
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2025-10-09 01:15:49 +0200

    chore(admin): update vite and typescript config

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,10 +1,22 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ["lucide-react"],
-  },
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return {
+    plugins: [react() /*, tailwindcss()*/],
+    define: {
+      "import.meta.env": {},
+    },
+    server: {
+      port: process.env.VITE_PORT ? parseInt(process.env.VITE_PORT) : 4200,
+      host: true,
+    },
+    base: "/",
+    optimizeDeps: {
+      exclude: ["lucide-react"],
+    },
+  };
 });
